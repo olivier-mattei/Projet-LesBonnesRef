d56d60d99c24db9b18086e46916c8232cd5d6bbf
Olivier <olimatt96@gmail.com>
2022-01-19 12:33:39 +0100

    Création du projet

---
+++ Back/LesBonnesRef/src/main/java/com/LesBonnesRef/demo/LesBonnesRefApplication.java
@@ -0,0 +1,13 @@
+package com.LesBonnesRef.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesBonnesRefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesBonnesRefApplication.class, args);
+	}
+
+}
